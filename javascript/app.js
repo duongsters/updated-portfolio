6db96ab9be0c518e5395a8b205dbea5ad701fdb3
Andrew Duong <duongsters@gmail.com>
2019-09-23 08:47:53 -0700

    created a bunch of timeout functions that will render the profile image of mine as you scrollbar down the webpage

--- javascript/app.js
+++ javascript/app.js
@@ -16,16 +16,45 @@ window.onscroll = function() {
             $("#animateHeader").css("box-shadow", "0px 0px 20px rgba(0, 0, 0, 0.085)");
             //change the padding property of 'animateHeader' to 
             //source: https://web-design-weekly.com/2014/11/18/viewport-units-vw-vh-vmin-vmax/
-            $("#animateHeader").css("padding", "5vh 5vw");
+            $("#animateHeader").css("padding", "3vh 3vw");
             //change the css stylings of the navbar, along with the 'a' tags within
             $("#navBar a").hover(function(){
                 //when this function runs, it will change the border-bottom property of the navBar to blue
                 $(this).css("border-bottom", "2px solid rgb(89, 45, 255)");
-                //then the 2nd function is ran and changes the blue navbar to transprent
-            }, function (){
+                //then the 2nd function is ran and changes the blue navbar to transprent colored with a dissapearing effect as user scrolls further down webpage
+            }, function() {
                 $(this).css("border-bottom", "2px solid transparent")
             }
+            else {
+                $("#animateHeader").css("background", "transparent");
+                $("#animateHeader").css("color", "#fff");
+                $("#animateHeader").css("box-shadow", "0px 0px 0px rgba(0, 0, 0, 0)");
+                $("#animateHeader").css("padding", "5vh 3vw");
+                $("#navBar a").hover(function(){
+                    $(this).css("border-bottom", "2px solid transparent");
+                });
+            }   
+            }
+//function that will animate zooming in the profile picture of mine as the user scrolls down the webpage
+        function ZoomImg(imgUrl) {
+        $("#imgPlaced").css("background", `url('${imgUrl}') center center`);
+        $("#zoomImg").css("display", "flex");
+        $("#zoomImg").addclass("animed zoomIn");
+        setTimeout(function(){
+            $("#zoomImg").removeClass("animated zoomIn");
+        }, 800);
+
+        function renderZoomImg(){
+            $("#zoomImg").addclass("animated zoomOut");
+            setTimeout(function(){
+                $("#zoomImg").css("display", "none");
+                $("#zoomImg").removeClass("animated zoomOut");
+                $("#imgPlaced").css("background", `url('') center center`);
+            }, 800);
+        }
+        setTimeout(function)
             });
+        }
 
         }
     }
